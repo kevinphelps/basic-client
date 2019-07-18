8f9672de7349b9590ecb2b14e1aafe8df8ad2ada
Kevin Phelps <kevinphelps@users.noreply.github.com>
2019-07-18 12:08:11 -0500

    chore: remove e2e tests

--- build/test.ts
+++ build/test.ts
@@ -1,30 +1,22 @@
 import { execute } from './helpers/shell.helpers';
-import { bailIf, parseFlags } from './helpers/utility.helpers';
+import { parseFlags } from './helpers/utility.helpers';
 
 interface Options {
   coverage: boolean;
   sourcemaps: boolean;
   watch: boolean;
-  e2e: boolean;
 }
 
 const defaultOptionsFn = (args: Options) => ({
   coverage: false,
   sourcemaps: args.coverage,
-  watch: false,
-  e2e: !args.watch
+  watch: false
 });
 
 const options = parseFlags(process.argv.slice(2), defaultOptionsFn);
 
-bailIf(options.watch && options.e2e, '--watch and --e2e are mutually exclusive');
-
 (async () => {
   await execute(getUnitTestCommand());
-
-  if (options.e2e) {
-    await execute('ng e2e');
-  }
 })();
 
 function getUnitTestCommand() {
